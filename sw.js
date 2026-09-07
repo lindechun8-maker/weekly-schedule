f379c283980ba33b72bef0e23e8535679d010ee6
lindechun8-maker <lindechun8-maker@users.noreply.github.com>
2026-09-07 22:23:25 +0800

    Restore detailed mobile weekly timetable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weekly-schedule-v4';
+const CACHE = 'weekly-schedule-v5';
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if(event.request.method === 'GET') event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
