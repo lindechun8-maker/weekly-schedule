23b824779874f09a71f4503950ef8f418669054a
lindechun8-maker <lindechun8-maker@users.noreply.github.com>
2026-09-07 21:54:43 +0800

    Fix cloud sync login and PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'weekly-schedule-v2';
+const CACHE = 'weekly-schedule-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache => cache.put(event.request,copy)); return response; }).catch(() => caches.match(event.request))); });
